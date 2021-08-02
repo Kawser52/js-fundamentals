03fb64a0e25b6d48317679712521aa732b0efaa8
Soriful Islam Kawser <shoriful521399@gmail.com>
2021-08-02 11:23:16 +0600

    array index

---
+++ array-index.js
@@ -0,0 +1,12 @@
+var programmerName=['Hablu', 'Boltu', 'jablu', 'Goltu', 'Kita'];
+// console.log(programmerName);
+var KitaIndex= programmerName.indexOf('Kita');
+// console.log(KitaIndex);
+
+var numberList= [1, 2, 3, 4];
+// console.log(numberList.indexOf(152));
+
+numberList [4]=45; 
+// console.log(numberList[2]);
+console.log(numberList);
+
